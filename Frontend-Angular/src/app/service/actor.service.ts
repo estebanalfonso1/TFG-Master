05c1f990294b6c838c63664fc3c03620f82dd8b0
estebanalfonso1 <alfonsoisnardesteban@gmail.com>
2025-05-27 19:42:17 +0200

    Cambio en CORS

--- Frontend-Angular/src/app/service/actor.service.ts
+++ Frontend-Angular/src/app/service/actor.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActorLogin } from '../model/ActorLogin';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
